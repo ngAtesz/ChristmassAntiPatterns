b3003feb27bee74529054261cd9daa22ee48744f
ngAtesz <molnar45@hotmail.com>
2015-12-17 08:26:03 +0100

    What to do

---
+++ AntiPatterns_avagy_Tiszta_kod_a_gatyam.py
@@ -0,0 +1,22 @@
+"""
+Felesleges vagy nem odaillő komment:
+     Azt hitte ő Gitrai Tamás, beállt a kommentátor állásba
+     Dennis a commit. [PyCharm-ban Version Control - Log]
+
+Inget -> Ezt nem tűröm tovább.
+
+Hosszú metódus:
+     Én vagyok a falu hossza
+
+Reggeli merevedés -> daily stand up
+
+Túl sok paraméter:
+     Paraméter, lidércnyomás mértékegységek.
+     Ez a kód sokkal karbantarthatóbb!
+
+Spaghetti Code:
+     Hány sör kódot ittam?
+     SZDD -> Szesz Driven Development
+
+Rossz elnevezések:
+"""
